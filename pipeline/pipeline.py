b4820cd4da0bc3a1cd5e0d1864c87a3089b11377
root <root@rri-lee15.robarts.uwo.ca>
2026-01-16 14:54:46 -0500

    created postgres database

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,7 +4,9 @@ print("arguments", sys.argv)
 
 day = int(sys.argv[1])
 
-df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df = pd.DataFrame({"week": [1, 2], "num_passengers": [3, 4]})
+df['day'] = day
+
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
